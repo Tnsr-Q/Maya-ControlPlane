e4cfca18510e5a019dfa62a71138db12850adcc4
Tnsr-Q <quantquiplabs@gmail.com>
2025-08-26 05:54:25 -0500

    Add missing hub __init__.py for proper package structure

---
+++ hub/__init__.py
@@ -0,0 +1,8 @@
+"""
+Maya-ControlPlane Hub Module
+
+Core orchestration and scheduling components for the Maya control plane system.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Maya-ControlPlane Team"
